4b2ad829aa6b55dd19136d25c2a4a72be055f68d
Leonardo Assunção <leonardoassuncao@deltaglobal.com.br>
2024-06-30 21:48:30 -0300

    Fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./utils/auth";
 import LogoutButton from "@/components/shared/LogoutButton";
--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,7 +1,7 @@
 import type { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import EmailProvider from "next-auth/providers/email";
-import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "./prisma";
 
 export const authOptions = {
